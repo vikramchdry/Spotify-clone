0afef0a631fa069a1f80044824f13fe8b6ba7ee5
Vikram Singh <ec1029231058@gmail.com>
2021-01-14 12:29:34 +0100

    login page and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="app">
-      <div>Welcome to Music App!</div>
+
+      <div>
+        <Login />
+      </div>
 
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/Login.css';
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+            <a>LOGIN TO SPOTIFY</a>
+
+            {/* spotify logo */}
+            {/*login with spotify buttons*/}
+
+        </div>
+    )
+}
+
+export default Login;
